9f6bf439e40dfe90ccb79a43500ffc7522247e1d
kiprox <70131116+kiprox@users.noreply.github.com>
2020-11-12 18:42:17 +0700

    Update generate.php

--- generate.php
+++ generate.php
@@ -3,6 +3,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Generator CFPlayer</title>
 	<link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.min.css" type="text/css" />
